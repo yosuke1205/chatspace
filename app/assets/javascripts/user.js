ad10709a3cee0335ed093e9da2b35528e0a843ba
yosuke1205 <yosuke.takeuchi@di-v.co.jp>
2019-03-14 16:54:29 +0900

    Summary

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -1,28 +1,30 @@
 $(document).on('turbolinks:load', function(){
 $(function() {
 function buildHTML(user) {
-   var html =`<div class="chat-group-user clearfix">
+  var html =
+  `<div class="chat-group-user clearfix">
   <p class="chat-group-user__name">${ user.name }</p>
   <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${ user.id }" data-user-name="${ user.name }">追加</a>
-</div>
-`
-    return html;
- }
+  </div>`
+  return html;
+}
 
 function removeName(userName, userId){
-  var html= `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-8'>
+  var html=
+  `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-8'>
   <input name='group[user_ids][]' type='hidden' value='${ userId }'>
   <p class='chat-group-user__name'>${ userName }</p>
   <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
-</div>`
-$("#user-add-result").append(html);
+  </div>`
+  $("#user-add-result").append(html);
 }
 
 function appendErrMsgToHTML(msg) {
-    var html = `<li>
-                  <div class="chat-group-user clearfix">${ msg }</div>
-                </li>`
-  }
+  var html =
+  `<li>
+  <div class="chat-group-user clearfix">${ msg }</div>
+  </li>`
+}
 
 $(document).on("click",'.user-search-add', function(){
   $(this).parent().remove();
@@ -32,7 +34,7 @@ $(document).on("click",'.user-search-add', function(){
 });
 
 $(document).on("click",'.user-search-remove',function(){
- $(this).parent().remove();
+  $(this).parent().remove();
 })
 
 
@@ -51,12 +53,12 @@ $("#user-search-field").on("keyup", function() {
       users.forEach(function(user){
         var html = buildHTML(user);
     $('#user-search-result').append(html)
-        });
-      }
+    });
+  }
     else {
       appendErrMsgToHTML("一致するユーザはいません");
-        }
-      })
+    }
+  })
   .fail(function() {
     alert('ユーザー検索に失敗しました');
       });
