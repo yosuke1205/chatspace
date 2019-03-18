617c669047bd914da16ffa96716cdd2ae507efca
yosuke1205 <yosuke.takeuchi@di-v.co.jp>
2019-03-18 18:55:00 +0900

    sssss

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "~> 3.11.0"
 
 set :application, "chatspace"
-set :repo_url, "git@example.com:yosuke1205/chatspace.git"
+set :repo_url, "git@github.com:yosuke1205/chatspace.git"
 
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
